8f608495feb2dad89a4b4288db1a3b219c4b3431
jovenpadawan <pedromiguelreyes.ne@gmail.com>
2026-07-11 17:47:12 -0500

    Reparación de enrutamientos

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProduction = process.env.NODE_ENV === 'production'
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: isProduction ? '/DEBAK_PAGE/' : '/',
+  base: './', 
   plugins: [react()],
   server: {
     host: '0.0.0.0',
