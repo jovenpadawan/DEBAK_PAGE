dce1e4ecadd51c4406c80466560b261952096af3
jovenpadawan <pedromiguelreyes.ne@gmail.com>
2026-07-11 17:31:46 -0500

    Ajustar base URL para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isProduction ? '/DEBAK_PAGE/' : '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
